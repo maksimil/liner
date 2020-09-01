3745095cdc5821f69e833f50fd228ead5af00ad9
Maxim <maxim.kosterov@gmail.com>
2020-09-01 20:03:48 +0300

    added command for init

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "Hello world";
+  if (argc == 1)
+  {
+  }
+  else
+  {
+    if (strcmp(argv[1], "init") == 0)
+    {
+      std::ofstream config("config.lua");
+      config << "settings = {\n";
+      config << "    tick_period = 100\n";
+      config << "}\n";
+      config << "\n";
+      config << "shape = {\n";
+      config << "    path = \"f.lua\",\n";
+      config << "    vname = \"shape\"\n";
+      config << "}\n";
+      config << "\n";
+      config << "updater = {\n";
+      config << "    path = \"f.lua\",\n";
+      config << "    vname = \"upd\"\n";
+      config << "}\n";
+      config.close();
+
+      std::ofstream f("f.lua");
+      f << "shape = {}\n";
+      f << "\n";
+      f << "upd = function(obj)\n";
+      f << "    print(\"HW\")\n";
+      f << "end\n";
+      f.close();
+    }
+  }
   return 0;
 }
